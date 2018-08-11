2ebd10672ba89049a7ad523568c4c82254963715
Administrator <Administrator@DEV25JHJAWIN>
2018-08-10 20:41:26 -0400

    Connection success

--- src/DBSettings.js
+++ src/DBSettings.js
@@ -1,10 +1,7 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('BattleshipDB', 'sa', 'Jcj41994', {
+const sequelize = new Sequelize('BattleshipDB', 'sa', 'Abc123', {
   host: 'localhost',
   dialect: 'mssql',
-  
-  server: "DEV25JHJAWIN",
-  domain: "DEV25JHJAWIN",
   pool: {
     max: 5,
     min: 0,
@@ -14,6 +11,7 @@ const sequelize = new Sequelize('BattleshipDB', 'sa', 'Jcj41994', {
   dialectOptions: {
     encrypt: false
   },
+  storage: './data.mssql',
   operatorsAliases: false
 });
 
@@ -25,3 +23,19 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+  const User = sequelize.define('user', {
+    username: Sequelize.STRING,
+    birthday: Sequelize.DATE
+  });
+  
+  sequelize.sync()
+    .then(() => User.create({
+      username: 'janedoe',
+      birthday: new Date(1980, 6, 20)
+    }))
+    .then(jane => {
+      console.log(jane.toJSON());
+    });
+
+module.exports = sequelize;
